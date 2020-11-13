d2fa4a6e4141b46319f3a39324ce116ea3205412
Rafael Nunes <rafaelscnunes@gmail.com>
2020-11-13 20:43:45 -0300

    change to use system python

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cd /Users/rafaelscnunes/Documents/workspace/automation/covid-19
-source .venv/bin/activate
+# cd /Users/rafaelscnunes/Documents/workspace/automation/covid-19
+# source .venv/bin/activate
 python get_covid_br_data.py
